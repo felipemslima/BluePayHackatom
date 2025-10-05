304e1f501bf5c8d32e98fa5f7f407b9f414896d1
Rennan Santos Nascimento <rennansantos.dev@gmail.com>
2025-10-04 21:04:20 -0300

    Delete wallet_service directory

--- wallet_service/gen_keys.py
+++
@@ -1,8 +0,0 @@
-from nacl.signing import SigningKey
-from nacl.encoding import Base64Encoder
-
-sk = SigningKey.generate()
-pk = sk.verify_key
-
-print("SERVER_SK_B64=", sk.encode(encoder=Base64Encoder).decode())
-print("SERVER_PK_B64=", pk.encode(encoder=Base64Encoder).decode())
